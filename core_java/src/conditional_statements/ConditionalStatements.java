02788fea9ea258aac38e459c7d028b201e79d655
Shibasish China <shibasish100@gmail.com>
2023-04-03 17:18:39 +0530

    first commit

---
+++ core_java/src/conditional_statements/ConditionalStatements.java
@@ -0,0 +1,14 @@
+package conditional_statements;
+
+public class ConditionalStatements 
+{
+  public static void main(String[] args) {
+	  System.out.println("if-else ");
+	  
+	  String name = "Guru";
+	   
+	  if(name == "guru") {
+		  System.out.println("Welcome ");
+	  }
+  }
+}
---
+++ core_java/src/o1_datatypes_and_varaibles/Varibles_and_datatypes.java
@@ -0,0 +1,29 @@
+package o1_datatypes_and_varaibles;
+
+public class Varibles_and_datatypes {
+	public static void main(String[] args) {
+	        System.out.println("Hello world");
+	        // eight data types in java
+	        // variable declaration and initialization in one line
+	        int a = 10;
+	        float f= 23.33f;
+	        double d = 23423.43;
+	        char ch = 'a';
+	        String s = "hello world of java.";
+	        String s1 = new String("Hello from new operator.");
+	        boolean result = false;
+	        short st = 1;
+	        byte be = 3;
+	        long  l1 = 1234567891;
+	        System.out.println(a);
+	        System.out.println(f);
+	        System.out.println(d);
+	        System.out.println(ch);
+	        System.out.println(s);
+	        System.out.println(s1);
+	        System.out.println(result);
+	        System.out.println(st);
+	        System.out.println(be);
+	        System.out.println(l1);
+	}
+}
